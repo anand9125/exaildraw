cb8543d2840232640f838bf5e00a7221b79a5016
Anand Chaudhary <achaudharyskn@gmail.com>
2026-02-15 18:41:57 +0530

    fix:remove frontend cause conflict added CI

--- apps/frontend/components/room/CreateForm.tsx
+++
@@ -1,9 +0,0 @@
-const CreateForm = () => {
-  return (
-    <div>
-      Enter
-    </div>
-  );
-}
-
-export default CreateForm;
---
+++ frontend/lib.ts
@@ -0,0 +1,2 @@
+export const NEXT_PUBLIC_HTTP_URL="http://localhost:3001"
+export const NEXT_PUBLIC_WS_URL="ws://localhost:8080"
